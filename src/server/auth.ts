6542109d5c0e492912b15af9bec46617416a5bf4
lloydrichards <lloyd.d.richards@gmail.com>
2024-05-03 18:06:37 +0200

    feat(auth): :passport_control: add role based authentication

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,13 +1,55 @@
 import { env } from "@/env";
-import NextAuth, { type NextAuthConfig } from "next-auth";
+import NextAuth, { type NextAuthConfig, type DefaultSession } from "next-auth";
 import GitHub from "next-auth/providers/GitHub";
 import Credentials from "next-auth/providers/credentials";
 
 export const BASE_PATH = "/api/auth";
 
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      role: UserRole;
+    } & DefaultSession["user"];
+  }
+  interface User {
+    role: UserRole;
+  }
+}
+
+type UserRole = "ADMIN" | "USER";
+export const ROLE: Record<UserRole, UserRole> = {
+  ADMIN: "ADMIN",
+  USER: "USER",
+};
+
 export const authConfig = {
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    session({ session, token }) {
+      session.user.role = token.role as UserRole;
+      return session;
+    },
+  },
   providers: [
-    GitHub,
+    GitHub({
+      profile(profile) {
+        return {
+          ...profile,
+          id: profile.id.toString(),
+          role: ROLE.USER,
+        };
+      },
+    }),
     Credentials({
       name: "Credentials",
       credentials: {
@@ -20,6 +62,7 @@ export const authConfig = {
             id: "sudo",
             userName: "admin",
             name: "Admin Mode",
+            email: "admin@outlier.com",
             password: env.AUTH_ADMIN_PASSWORD,
           },
         ];
@@ -28,7 +71,7 @@ export const authConfig = {
             user.userName === credentials.username &&
             user.password === credentials.password,
         );
-        return user ? { id: user.id, name: user.name } : null;
+        return user ? { ...user, role: ROLE.ADMIN } : null;
       },
     }),
   ],
