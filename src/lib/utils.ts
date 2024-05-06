afd1a88c339092b93c07e52a48627bc8583c203f
lloydrichards <lloyd.d.richards@gmail.com>
2024-05-06 18:37:09 +0200

    fix(components): :bug: use locale time, not utc

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,11 @@
 import { type ClassValue, clsx } from "clsx";
-import { utcFormat } from "d3-time-format";
+import { timeFormat } from "d3-time-format";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const formatTimeWithMeridiem = utcFormat("%I:%M %p");
-export const formatTime = utcFormat("%I:%M");
-export const formatDate = utcFormat("%a %b %d");
+export const formatTimeWithMeridiem = timeFormat("%I:%M %p");
+export const formatTime = timeFormat("%I:%M");
+export const formatDate = timeFormat("%a %b %d");
