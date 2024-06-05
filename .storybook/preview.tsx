5093e3a17dee44ac61c2b57c9f7f0628163948f8
lloydrichards <lloyd.d.richards@gmail.com>
2024-06-05 18:19:39 +0200

    test(storybook): :globe_with_meridians: add i18n to storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,14 +4,19 @@ import { cn } from "../src/lib/utils";
 import "../src/styles/globals.css";
 import { silka } from "../src/styles/font";
 import { TRPCReactProvider } from "../src/trpc/react";
+import { NextIntlClientProvider } from "next-intl";
+
+import messages from "../messages/en.json";
 
 export const decorators: Decorator[] = [
   (Story) => {
     return (
       <TRPCReactProvider>
-        <main className={cn("font-silka", silka.variable)}>
-          <Story />
-        </main>
+        <NextIntlClientProvider locale="en" messages={messages}>
+          <main className={cn("font-silka", silka.variable)}>
+            <Story />
+          </main>
+        </NextIntlClientProvider>
       </TRPCReactProvider>
     );
   },
