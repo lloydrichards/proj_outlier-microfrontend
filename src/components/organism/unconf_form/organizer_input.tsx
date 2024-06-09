b09ad92605a74efd1539201366ab325646d70ca1
lloydrichards <lloyd.d.richards@gmail.com>
2024-06-09 17:56:35 +0200

    fix(forms): :bug: tweaks to the UnconfForm

--- src/components/organism/unconf_form/organizer_input.tsx
+++ src/components/organism/unconf_form/organizer_input.tsx
@@ -22,7 +22,7 @@ export const OrganizerInput: FC<{ index: number }> = ({ index }) => {
         name={`organizers.${index}.firstName`}
         render={({ field }) => (
           <FormItem>
-            <FormLabel>{t("last_label")}</FormLabel>
+            <FormLabel>{t("first_label")}</FormLabel>
             <FormControl>
               <Input
                 placeholder={t("first_placeholder")}
