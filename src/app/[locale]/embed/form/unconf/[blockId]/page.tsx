b13f2fea46222502c01399a61cd6c636afe2b313
lloydrichards <lloyd.d.richards@gmail.com>
2024-06-09 18:31:10 +0200

    refactor(components): :recycle: add LocaleSwitcher to Unconf form

--- src/app/[locale]/embed/form/unconf/[blockId]/page.tsx
+++ src/app/[locale]/embed/form/unconf/[blockId]/page.tsx
@@ -1,3 +1,4 @@
+import { LocaleSwitcher } from "@/components/molecule/locale_switcher/locale_switcher";
 import { UnconfForm } from "@/components/organism/unconf_form/unconf_form";
 import { api } from "@/trpc/server";
 import { notFound } from "next/navigation";
@@ -14,8 +15,9 @@ export default async function UnconfFormPage({
     return notFound();
   }
   return (
-    <section className="min-h-screen px-4 py-8">
+    <section className="relative min-h-screen px-4 py-14">
       <UnconfForm block={block} />
+      <LocaleSwitcher className="absolute left-3 top-2 opacity-80" />
     </section>
   );
 }
